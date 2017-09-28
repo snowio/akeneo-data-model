3aa7454c84a042c6652ab38384c96206a553d4e9
alexwanyoike <wanyoike.alexander@gmail.com>
2017-09-28 07:26:37 +0100

    Add Family and AttributeGroup VO Add FamilySavedEvent

---
+++ src/AttributeGroup.php
@@ -0,0 +1,41 @@
+<?php
+namespace SnowIO\AkeneoDataModel;
+
+class AttributeGroup
+{
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function getLabels(): array
+    {
+        return $this->labels;
+    }
+
+    public function getLabel(string $locale): string
+    {
+        return $this->labels[$locale] ?? null;
+    }
+
+    public function getSortOrder(): int
+    {
+        return $this->sortOrder;
+    }
+
+    public static function fromJson(array $json): self
+    {
+        $attributeGroup = new self;
+        $attributeGroup->code = $json['code'];
+        $attributeGroup->labels = $json['labels'];
+        $attributeGroup->sortOrder = $json['sort_order'];
+    }
+
+    private $code;
+    private $labels;
+    private $sortOrder;
+
+    private function __construct()
+    {
+    }
+}

7b663c3b2420eb25b20f3e3236da728c84a565fc
Josh Di Fabio <joshdifabio@gmail.com>
2017-10-18 09:21:50 +0100

    Add Price object

---
+++ src/Price.php
@@ -0,0 +1,29 @@
+<?php
+namespace SnowIO\AkeneoDataModel;
+
+class Price
+{
+    public static function of(string $amount, string $currency): self
+    {
+        return new self($amount, $currency);
+    }
+
+    public function getAmount(): string
+    {
+        return $this->amount;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    private $amount;
+    private $currency;
+
+    private function __construct(string $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+}
--- src/PriceSet.php
+++ src/PriceSet.php
@@ -3,46 +3,73 @@ namespace SnowIO\AkeneoDataModel;
 
 class PriceSet implements \IteratorAggregate
 {
-    public function hasPrice(string $currencyCode): bool
+    public static function of(array $prices): self
     {
-        return isset($this->prices[$currencyCode]);
+        $pricesByCurrency = [];
+        foreach ($prices as $price) {
+            if (!$price instanceof Price) {
+                throw new \Error;
+            }
+            if (isset($pricesByCurrency[$price->getCurrency()])) {
+                throw new \Error;
+            }
+            $pricesByCurrency[$price->getCurrency()] = $price;
+        }
+        return new self($pricesByCurrency);
     }
 
-    public function getPrice(string $currencyCode): ?string
+    public function hasPrice(string $currency): bool
     {
-        return $this->prices[$currencyCode] ?? null;
+        return isset($this->prices[$currency]);
+    }
+
+    public function getPrice(string $currency): ?Price
+    {
+        return $this->prices[$currency] ?? null;
+    }
+
+    public function withPrice(Price $price): self
+    {
+        $result = clone $this;
+        $result->prices[$price->getCurrency()] = $price;
+        return $result;
+    }
+
+    public function getAmount(string $currency): ?string
+    {
+        return isset($this->prices[$currency]) ? $this->prices[$currency]->getAmount() : null;
+    }
+
+    public function filter(callable $predicate): self
+    {
+        $prices = \array_filter($this->prices, $predicate);
+        return new self($prices);
     }
 
     public static function fromJson(array $json): PriceSet
     {
         $prices = [];
-        foreach ($json as $currencyCode => $price) {
+        foreach ($json as $currencyCode => $amount) {
             if (!\is_string($currencyCode)) {
                 throw new \Error;
             }
-            if ($price === null) {
+            if ($amount === null) {
                 continue;
             }
-            if (!\is_string($price)) {
+            if (!\is_string($amount)) {
                 throw new \Error;
             }
-            $prices[$currencyCode] = $price;
+            $prices[$currencyCode] = Price::of($currencyCode, $amount);
         }
         return new PriceSet($prices);
     }
 
     public function getIterator(): \Iterator
     {
-        foreach ($this->prices as $currencyCode => $price) {
-            yield $currencyCode => $price;
-        }
+        yield from \array_values($this->prices);
     }
 
-    public function toArray(): array
-    {
-        return $this->prices;
-    }
-
+    /** @var Price[] */
     private $prices;
 
     private function __construct(array $prices)

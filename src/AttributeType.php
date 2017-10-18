93ce67c15550c21ec4d696470458722d351a5989
Josh Di Fabio <joshdifabio@gmail.com>
2017-10-18 15:43:34 +0100

    Make AttributeType constants less verbose

--- src/AttributeType.php
+++ src/AttributeType.php
@@ -3,19 +3,24 @@ namespace SnowIO\AkeneoDataModel;
 
 abstract class AttributeType
 {
-    const PIM_CATALOG_IDENTIFIER = 'pim_catalog_identifier';
-    const PIM_CATALOG_SIMPLESELECT = 'pim_catalog_simpleselect';
-    const PIM_CATALOG_BOOLEAN = 'pim_catalog_boolean';
-    const PIM_CATALOG_NUMBER = 'pim_catalog_number';
-    const PIM_CATALOG_PRICE_COLLECTION = 'pim_catalog_price_collection';
-    const PIM_CATALOG_MULTISELECT = 'pim_catalog_multiselect';
+    const IDENTIFIER = 'pim_catalog_identifier';
+    const SIMPLESELECT = 'pim_catalog_simpleselect';
+    const BOOLEAN = 'pim_catalog_boolean';
+    const NUMBER = 'pim_catalog_number';
+    const PRICE_COLLECTION = 'pim_catalog_price_collection';
+    const MULTISELECT = 'pim_catalog_multiselect';
 
     const ALL = [
-        self::PIM_CATALOG_IDENTIFIER,
-        self::PIM_CATALOG_SIMPLESELECT,
-        self::PIM_CATALOG_BOOLEAN,
-        self::PIM_CATALOG_NUMBER,
-        self::PIM_CATALOG_PRICE_COLLECTION,
-        self::PIM_CATALOG_MULTISELECT
+        self::IDENTIFIER,
+        self::SIMPLESELECT,
+        self::BOOLEAN,
+        self::NUMBER,
+        self::PRICE_COLLECTION,
+        self::MULTISELECT
     ];
+    
+    private function __construct()
+    {
+        
+    }
 }
